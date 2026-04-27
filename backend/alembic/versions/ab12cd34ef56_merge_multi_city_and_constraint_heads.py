a556ac3d052dd32bdbd7c8ad1013472a38db428b
Tirth-Prajapati-210804 <tirthprajapati214@gmail.com>
2026-04-27 23:04:58 +0530

    Merge Alembic migration heads

---
+++ backend/alembic/versions/ab12cd34ef56_merge_multi_city_and_constraint_heads.py
@@ -0,0 +1,22 @@
+"""merge_multi_city_and_constraint_heads
+
+Revision ID: ab12cd34ef56
+Revises: 9f8e7d6c5b4a, c8d9e0f1a2b3
+Create Date: 2026-04-27 23:25:00.000000
+
+"""
+from __future__ import annotations
+
+
+revision = "ab12cd34ef56"
+down_revision = ("9f8e7d6c5b4a", "c8d9e0f1a2b3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
